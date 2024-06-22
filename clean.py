16b97f34e1016591a110c94df725c91d65d7dff6
Alex Ross <adross1027@gmail.com>
2024-06-22 10:48:41 -0700

    added clean cron log

--- clean.py
+++ clean.py
@@ -1,5 +1,6 @@
 from helpers import read_posts_json, write_posts_json
-import time, datetime
+import time,  os, json
+from datetime import datetime
 
 # Clear out posts from posts.json that are NOT younger than 5 minutes
 def clean_posts():
@@ -8,14 +9,26 @@ def clean_posts():
 
     current_time = time.time()
 
-    # Filter out posts older than 5 minutes
-    posts = [post for post in posts if (current_time - post['time_posted']) < 300]
+    # Filter out posts older than a week (needs to be at least a weeek to avoid removing posts that can be viewed and sent again)
+    posts = [post for post in posts if (current_time - post['time_posted']) < 604800]
 
     write_posts_json(posts)
+
+def clean_log():
+    # Get full path
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    filename = os.path.join(dir_path, 'cron.log')
+
+    try:
+        with open(filename, 'w') as file:
+                json.dump('', file, indent=4)
+    except Exception:
+        print('cron.log not found') 
             
 def main():
     clean_posts()
+    clean_log()
     time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    print(f'{time} -> Cleaned out old posts!')
+    print(f'{time} -> Cleaned out old posts and logs!')
 
 main()
